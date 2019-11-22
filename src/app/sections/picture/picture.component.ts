6a7296fe253f03e887c21d525f3c23a8db4228cf
Pascal Stockert <pascal.stockert@live.com>
2019-11-22 13:05:24 +0100

    re-added picture component

--- src/app/sections/picture/picture.component.ts
+++ src/app/sections/picture/picture.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-picture',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PictureComponent implements OnInit {
 
+  @Input() sectionData;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.sectionData);
   }
 
 }
