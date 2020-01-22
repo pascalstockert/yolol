50ed3c43976e25b9b9d54a69a1facc6c4f7f1275
Pascal Stockert <pascal.stockert@live.com>
2020-01-22 23:57:37 +0100

    added new prismic pipe and fixed remaining console errors

---
+++ src/app/components/pipes/prismic-pipe/prismic-domtext.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PrismicDOMtextPipe } from './prismic-domtext.pipe';
+
+describe('PrismicDOMtextPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrismicDOMtextPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
