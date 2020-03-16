513a02f1d22175773b34369ef644805ba8f2d00c
Pascal Stockert <pascal.stockert@live.com>
2020-03-16 22:31:01 +0100

    removed parallax-effect from header-slice; parallax is not for me

--- src/app/sections/header-slice/header-slice.component.ts
+++ src/app/sections/header-slice/header-slice.component.ts
@@ -20,15 +20,6 @@ export class HeaderSliceComponent implements OnInit {
     if (this.background_img === undefined) {
       this.background_img = 'https://cobaltcrowproductions.xyz/image/53289-full_backgrounds-1920x1080-wallpaper-cave.jpg';
     }
-
-    const slice = document.querySelector('#header-slice');
-    const headerContent = document.querySelector('.abs-center');
-    this.scrollService.scroll$.subscribe(pos => {
-      // @ts-ignore
-      slice.style.transform = 'translateY(-' + pos / 2 + 'px)';
-      // @ts-ignore
-      headerContent.style.opacity = 1 - window.scrollY / 300;
-    });
   }
 
 }
