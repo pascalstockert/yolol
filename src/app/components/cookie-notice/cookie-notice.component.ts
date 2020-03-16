7604077526f694b399241f9e0f5ab72ac0c96252
Pascal Stockert <pascal.stockert@live.com>
2020-03-16 23:28:10 +0100

    adjusted cookie notice animation

--- src/app/components/cookie-notice/cookie-notice.component.ts
+++ src/app/components/cookie-notice/cookie-notice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { faCookieBite, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,15 +6,33 @@ import { faCookieBite, faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './cookie-notice.component.html',
   styleUrls: ['./cookie-notice.component.scss']
 })
-export class CookieNoticeComponent implements OnInit {
+export class CookieNoticeComponent implements OnInit, AfterViewInit {
 
   hidden = false;
   cookie = faCookieBite;
   times = faTimes;
+  cookieNotice: HTMLElement;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.cookieNotice = document.getElementById('cookie-notice');
+    this.enter();
+  }
+
+  enter() {
+    setTimeout(() => {
+      this.cookieNotice.style.opacity = '1';
+      this.cookieNotice.style.transform = 'scale(1, 1)';
+      this.cookieNotice.style.bottom = '30px';
+    }, 500);
+  }
+
+  click() {
+    this.cookieNotice.style.display = 'none';
   }
 
 }
