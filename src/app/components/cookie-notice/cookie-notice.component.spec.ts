84db2baf475036250dc3a329fea8628dfd27bc3d
Pascal Stockert <pascal.stockert@live.com>
2021-05-05 00:37:18 +0200

    Update angular cli

--- src/app/components/cookie-notice/cookie-notice.component.spec.ts
+++ src/app/components/cookie-notice/cookie-notice.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CookieNoticeComponent } from './cookie-notice.component';
 
@@ -6,7 +6,7 @@ describe('CookieNoticeComponent', () => {
   let component: CookieNoticeComponent;
   let fixture: ComponentFixture<CookieNoticeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CookieNoticeComponent ]
     })
--- src/app/sections/quote/quote.component.spec.ts
+++ src/app/sections/quote/quote.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { QuoteComponent } from './quote.component';
 
@@ -6,7 +6,7 @@ describe('QuoteComponent', () => {
   let component: QuoteComponent;
   let fixture: ComponentFixture<QuoteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ QuoteComponent ]
     })
