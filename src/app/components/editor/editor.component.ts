e61bf69a2494278fe901a3523425166f560d10f0
Pascal Stockert <pascal.stockert@live.com>
2021-05-29 12:17:57 +0200

    Resolve conflicts

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -191,4 +191,9 @@ export class EditorComponent implements OnInit, AfterViewInit {
     this.chipIntervalSubscription.unsubscribe();
   }
 
+  getCaretPosition(): void {
+    const { top , left } = window.getSelection().getRangeAt(0).getClientRects()[0];
+    console.log( { top, left } );
+  }
+
 }
