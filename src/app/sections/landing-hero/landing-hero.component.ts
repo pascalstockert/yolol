cf857335c8cfa9e5d063d4b4691f03470f19b172
Pascal Stockert <pascal.stockert@live.com>
2023-04-27 22:32:32 +0200

    Fix several usability issues

--- src/app/sections/landing-hero/landing-hero.component.ts
+++ src/app/sections/landing-hero/landing-hero.component.ts
@@ -11,28 +11,11 @@ export class LandingHeroComponent implements OnInit {
   @Input() sectionData;
 
   public backgroundImg: any;
-  voices;
-  voiceTriplet = [];
 
   constructor( public router: Router ) {}
 
   ngOnInit(): void {
-    this.voices = this.sectionData.items;
-    this.voiceTriplet = this.shuffleVoices();
     this.backgroundImg = this.sectionData.primary.background_image.url;
-  }
-
-  shuffleVoices(): any[] {
-    this.voices = this.shuffle( this.voices );
-    return this.voices.slice(0, Math.min(this.voices.length, 3));
-  }
-
-  shuffle(a: any): any[] {
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
   }
 
 }
